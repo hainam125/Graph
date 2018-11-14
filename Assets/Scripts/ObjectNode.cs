aaa5f46d8fccc19e4d763e01e13b1b0c7c75dac9
GNT-GLOBAL\nam.nh2388 <nam.nh2388@gnt.com.vn>
2018-11-14 11:36:22 +0700

    Update Test

---
+++ Assets/Scripts/ObjectNode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectNode : MonoBehaviour {
+    public static HashSet<int> indexList = new HashSet<int>();
+    private static Color[] baseColorList = new Color[] {Color.cyan, Color.green, Color.red, Color.black, Color.yellow, Color.blue, Color.magenta, Color.gray, Color.white, Color.grey };
+    private static List<Color> colorList = new List<Color>();
+    public int index;
+    public List<ObjectNode> Connects;
+    private Color color = Color.black;
+
+    private void Awake()
+    {
+        SetColor();
+        string pattern = @"(\d+)";
+        Match m = Regex.Match(name, pattern, RegexOptions.IgnoreCase);
+        if (m.Success)
+        {
+            index = int.Parse(m.Value);
+            if(!indexList.Add(index)) throw new System.Exception("Duplicate index");
+        }
+        else {
+            throw new System.Exception("No index");
+        }
+    }
+
+    private void SetColor()
+    {
+        if (colorList.Count == 0) {
+            foreach (var c in baseColorList) colorList.Add(c);
+        }
+        int i = Random.Range(0, colorList.Count);
+        color = colorList[i];
+        colorList.RemoveAt(i);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Handles.Label(transform.position, index.ToString());
+        Gizmos.color = color;
+        for (int i = 0; i < Connects.Count; i++)
+        {
+            Gizmos.DrawLine(transform.position, Connects[i].transform.position);
+        }
+    }
+}
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,59 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour {
-	public List<Transform> nodeObjects;
+    public int startNode;
+    public int endNode;
 	private SparseGraph graph;
 
 	private void Start() {
 		CreateGraph();
-		DijkstraSearch();
+		//DijkstraSearch();
 		//BreadthFirstSearch();
 		//DepthFirstSearch();
 	}
 
 	private void CreateGraph() {
-		graph = new SparseGraph(true);
-		for (int i = 0; i < 6; i++) {
-			graph.AddNode(new GraphNode(i));
-		}
-		graph.AddEdge(new GraphEdge(0, 1));
-		graph.AddEdge(new GraphEdge(0, 2));
-		graph.AddEdge(new GraphEdge(1, 0));
-		graph.AddEdge(new GraphEdge(1, 4));
-		graph.AddEdge(new GraphEdge(2, 0));
-		graph.AddEdge(new GraphEdge(2, 3));
-		graph.AddEdge(new GraphEdge(3, 1));
-		graph.AddEdge(new GraphEdge(3, 4));
-		graph.AddEdge(new GraphEdge(3, 5));
-		graph.AddEdge(new GraphEdge(4, 3));
-		graph.AddEdge(new GraphEdge(4, 5));
-		graph.AddEdge(new GraphEdge(5, 4));
-		graph.AddEdge(new GraphEdge(5, 3));
+		graph = new SparseGraph(false);
+        var objects = FindObjectsOfType<ObjectNode>().OrderBy( x=>x.index).ToArray();
+        foreach (var o in objects) graph.AddNode(new GraphNode(o.index));
+        foreach(var o in objects)
+        {
+            foreach(var n in o.Connects)
+            {
+                graph.AddEdge(new GraphEdge(o.index, n.index, Vector2.Distance(o.transform.position, n.transform.position)));
+            }
+        }
 	}
 
 	private void DijkstraSearch() {
-		var dfs = new GraphSearchDijkstra(graph, 4, 2);
-		var path = dfs.GetPathToTarget();
-		string pathString = string.Empty;
-		foreach (var nodeIdx in path) pathString += nodeIdx + ";";
-		Debug.Log(pathString);
+		var dfs = new GraphSearchDijkstra(graph, startNode, endNode);
+        ShowLog(dfs.GetPathToTarget());
 	}
 
 	private void BreadthFirstSearch() {
-		var dfs = new GraphSerachBFS(graph, 4, 2);
-		var path = dfs.GetPathToTarget();
-		string pathString = string.Empty;
-		foreach (var nodeIdx in path) pathString += nodeIdx + ";";
-		Debug.Log(pathString);
+		var dfs = new GraphSerachBFS(graph, startNode, endNode);
+        ShowLog(dfs.GetPathToTarget());
 	}
 
 	private void DepthFirstSearch () {
-		var dfs = new GraphSearchDFS(graph, 4, 2);
-		var path = dfs.GetPathToTarget();
-		string pathString = string.Empty;
-		foreach (var nodeIdx in path) pathString += nodeIdx + ";";
-		Debug.Log(pathString);
-	}
+		var dfs = new GraphSearchDFS(graph, startNode, endNode);
+        ShowLog(dfs.GetPathToTarget());
+    }
+
+    private void ShowLog(List<int> path)
+    {
+        string pathString = string.Empty;
+        foreach (var nodeIdx in path) pathString += nodeIdx + ";";
+        Debug.Log(pathString);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            CreateGraph();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DijkstraSearch();
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            BreadthFirstSearch();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DepthFirstSearch();
+        }
+    }
 }
